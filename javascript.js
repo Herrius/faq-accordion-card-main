626bf097a60f599c3d4e17f9b6385115060ac6e3
Herrius <65200014+Herrius@users.noreply.github.com>
2021-04-19 21:53:41 -0500

    animacion de boton hacia abajo y arriba

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ arrow.forEach((item)=>{
     item.addEventListener('click', (event)=>{
         console.log("click!");
         item.nextElementSibling.classList.toggle('text-no-hiden');
+        item.childNodes[2].classList.toggle('round-down')
+        
     })
 })
 
